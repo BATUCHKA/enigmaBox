6c61756c504e3fe57683979169e365f91a5f1163
Yuuta <chinzoo_0628@yahoo.com>
2019-10-08 20:02:50 +0800

    YOTONandEDI push

--- public/js/skin.js
+++ public/js/skin.js
@@ -1,3 +1,32 @@
+const db = firebase.firestore();
+let userId;
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log(user);
+        userId = user.uid;
+
+        db.doc('Users/' + userId).get()
+            .then(function(doc) {
+                const lvl= doc.data().level;
+
+                if (lvl >= 2 && lvl < 4) {
+                    let lvl2BoxEl = document.getElementsByClassName('silverb')[0].src = '../src/silverb.png';
+                }
+                if (lvl >= 4 && lvl < 6) {
+                    let lvl2BoxEl = document.getElementsByClassName('silverb')[0].src = '../src/silverb.png';
+                    let lvl4BoxEl = document.getElementsByClassName('goldenb')[0].src = '../src/goldenb.png';
+                }
+                if (lvl == 6) {
+                    let lvl2BoxEl = document.getElementsByClassName('silverb')[0].src = '../src/silverb.png';
+                    let lvl4BoxEl = document.getElementsByClassName('goldenb')[0].src = '../src/goldenb.png';
+                    let lvl6BoxEl = document.getElementsByClassName('diamondb')[0].src = '../src/dimondb.png';
+                }
+            })
+    }
+});
+
+
 function jump() {
     window.location.href = `../profile.html`
 }
